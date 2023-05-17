1a528413169e4bb3c533d42e5cb8202e26b0c299
E-Code | Étienne S <sauvage-etienne@hotmail.com>
2023-05-17 21:04:58 +0200

    Create constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+export const FETCH_METHOD = {
+  GET_ONE: 'getOne',
+  GET_FULL_LIST: 'getFullList',
+  CREATE: 'create',
+  DELETE: 'delete',
+};
+
+export const COLLECTIONS = {
+  TEST: 'test',
+};
